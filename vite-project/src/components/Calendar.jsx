642d8a8af3dc847a4bfb7c834223d0625f187ea8
Mehrab Azan <77406365+MehrabAzan@users.noreply.github.com>
2026-06-11 03:54:28 -0500

    commit

---
+++ vite-project/src/components/Calendar.jsx
@@ -0,0 +1,126 @@
+import Event from './Event'
+
+const Calendar = () => {
+    return (
+        <div className="Calendar">
+            <table>
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>Sunday</th>
+                        <th>Monday</th>
+                        <th>Tuesday</th>
+                        <th>Wednesday</th>
+                        <th>Thursday</th>
+                        <th>Friday</th>
+                        <th>Saturday</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td className="time">8 am</td>
+                        <Event event='Fancy Dinner 🎩' color ='green' location='Maple & Ash'/>
+                        <Event event='Starbucks ☕️' color ='green'/>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <Event event='Yolk 🍳' color ='green'/>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className="time">9 am</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <Event event='Subway 🚊' color ='pink'/>
+                        <td></td>
+                        <td></td>
+                        <Event event='The Bean 🫘' color ='blue'/>
+                    </tr>
+                    <tr>
+                        <td className="time">10 am</td>
+                        <Event event='Art Institute' color='blue'/>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <Event event='Riverwalk' color='pink'/>
+                        <td></td>
+                        <Event event='Navy Pier' color='green'/>
+                    </tr>
+                    <tr>
+                        <td className="time">11 am</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className="time">12 pm</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className="time">1 pm</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className="time">2 pm</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className="time">3 pm</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className="time">4 pm</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className="time">5 pm</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default Calendar;
